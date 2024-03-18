79e2411ec7a1866dca126a6eb9abfa1f3ba4dc96
white-ras <rasras3366@gmail.com>
2024-03-18 11:34:01 +0900

    外観をとりあえず作成

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, Form, InputGroup, Navbar } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container className="mt-5">
+        <Navbar className="mb-5"bg="secondary" variant="light" expand="lg" fontSize="lg">
+          <h1>To Do List</h1>
+        </Navbar>
+
+
+        <InputGroup className="mb-5">
+          <Form.Control
+            placeholder="Todoを入力してください"
+            aria-label="Todoを入力してください"
+            aria-describedby="basic-addon2"
+          />
+          <Button variant="outline-secondary" id="add-button">
+            追加
+          </Button>
+        </InputGroup>
+
+        <hr />
+
+        <div class="mt-5" id="js-todo-list">
+
+          <InputGroup className="mb-3">
+            <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+            <Form.Control aria-label="Text input with checkbox" />
+            <Button variant="outline-secondary">編集</Button>
+            <Button variant="outline-secondary">削除</Button>
+          </InputGroup>
+
+          <InputGroup className="mb-3">
+            <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+            <Form.Control aria-label="Text input with checkbox" />
+            <Button variant="outline-secondary">編集</Button>
+            <Button variant="outline-secondary">削除</Button>
+          </InputGroup>
+
+          <InputGroup className="mb-3">
+            <Form.Control
+              placeholder="Todoを入力してください"
+              aria-label="Todoを入力してください"
+              aria-describedby="basic-addon2"
+            />
+            <Button variant="outline-secondary" id="add-button">
+              編集
+            </Button>
+          </InputGroup>
+
+
+          <InputGroup className="mb-3">
+            <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+            <Form.Control aria-label="Text input with checkbox" />
+            <Button variant="outline-secondary">編集</Button>
+            <Button variant="outline-secondary">削除</Button>
+          </InputGroup>
+
+        </div>
+
+
+        <Navbar className="mb-5"bg="secondary" variant="light" expand="lg" >
+          <p>全てのタスク：３ 完了済み：１ 未完了：２</p>
+        </Navbar>
+
+      </Container>
+  </>
   );
 }
 
