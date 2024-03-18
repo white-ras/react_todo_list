3ebe2e642db878dffb0798118a58d1154d47e596
white-ras <rasras3366@gmail.com>
2024-03-19 08:58:17 +0900

    checkboxの挙動を実装

--- src/components/molecules/TodoItem.jsx
+++ src/components/molecules/TodoItem.jsx
@@ -3,9 +3,12 @@ import { Button, Form, InputGroup } from "react-bootstrap"
 export const TodoItem = (props) => {
   const { todos, setTodos, id, text, completed = false } = props;
 
+  const textDecoration = completed === true ? "line-through" : "none";
+
   const hadleCheckboxChange = (event) => {
-    // alert(event.target);
-    // todos[id].completed = !completed;
+    const newTodos = [...todos];
+    newTodos[id].completed = !completed;
+    setTodos(newTodos);
   }
 
   const onClickDelete = () => {
@@ -14,20 +17,27 @@ export const TodoItem = (props) => {
     setTodos(newTodos);
   }
 
+  const onClickEdit = (event) => {
+    const inputElement = event.target.previousElementSibling;
+    inputElement.disabled=false;
+    // 前の要素を取得、disabled=false、deleteボタン削除、編集ボタン削除、保存ボタン追加。
+  }
+
   return (
     <InputGroup className="mb-3">
       <InputGroup.Checkbox
         aria-label="Checkbox for following text input"
         checked={completed}
-        onChange={hadleCheckboxChange(id)}
+        onChange={hadleCheckboxChange}
       />
 
       <Form.Control
         aria-label="Text input with checkbox"
         value={text}
         disabled="true"
+        style={{ textDecoration: textDecoration }}
       />
-      <Button variant="outline-secondary">編集</Button>
+      <Button variant="outline-secondary" onClick={onClickEdit}>編集</Button>
       <Button variant="outline-secondary" onClick={onClickDelete}>削除</Button>
     </InputGroup>
   )
